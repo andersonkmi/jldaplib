4849bbfb30e77a63e66e32d5116d495e88bcfb49
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-03-07 10:25:52 +0100

    more code

--- src/main/java/org/sharpsw/ldap/services/SearchScope.java
+++ src/main/java/org/sharpsw/ldap/services/SearchScope.java
@@ -1,8 +1,27 @@
 package org.sharpsw.ldap.services;
 
 public enum SearchScope {
-	SUBTREE,
-	ONE_LEVEL,
-	OBJECT,
-	NONE
+	SUBTREE("subtree"),
+	ONE_LEVEL("onelevel"),
+	OBJECT("object"),
+	NONE("none");
+
+	private final String code;
+
+	SearchScope(String code) {
+		this.code = code;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public static SearchScope findByCode(String code) {
+		for (SearchScope scope : values()) {
+			if (scope.code.equals(code)) {
+				return scope;
+			}
+		}
+		return NONE;
+	}
 }
